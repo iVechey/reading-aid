03d10977f58ab88c68b5b11b8b05b5b268ab84d8
borleyk <borleyk@wwu.edu>
2021-05-09 22:59:59 -0700

    Classes table now shows up on first load of homepage

--- teacher/src/ClassOverview.js
+++ teacher/src/ClassOverview.js
@@ -7,36 +7,66 @@ class ClassOverview extends React.Component {
     constructor(props) {
         super(props);
         this.renderRows = this.renderRows.bind(this);
+        this.renderTable = this.renderTable.bind(this);
         this.getClassrooms = this.getClassrooms.bind(this);
         this.state = {
             classrooms: [],
             user: firebase.auth().currentUser,
+            retrievedClassrooms: false,
         }
-        this.getClassrooms(this.state.user.uid);
     }
 
-    getClassrooms(user) {
-        firebase.database().ref('teachers').child(user).child('classrooms').on('value', (snapshot) => {
+    componentDidMount() {
+        this.getClassrooms();
+    }
+
+    getClassrooms() {
+        let ref = firebase.database().ref("teachers/" + this.state.user.uid + "/classrooms");
+        ref.once('value').then(snapshot => {
             if(snapshot.exists()) {
                 snapshot.forEach((child) => {
                     this.state.classrooms.push(child.val());
                 });
+                this.setState({retrievedClassrooms: true});
             }
         });
     }
 
     renderRows() {
         const classrooms = this.state.classrooms;
+        console.log(classrooms.length);
+        return classrooms.map((classroom, index) => {
+            return (
+                <tr role="button" tabIndex="0" data-index={index} key={"classroom-"+index} onClick={() => {this.props.showClassroom(classroom.code)}}>
+                        <td>{classroom.classroom_name}</td>
+                        <td>{classroom.code}</td>
+                        <td>{classroom.num_students}</td>
+                    </tr>
+                );
+            });
+        }
+        
+    renderTable() {
+        const classrooms = this.state.classrooms;
+        console.log(classrooms.length);
         if(classrooms.length > 0) {
-            return classrooms.map((classroom, index) => {
-                    return (
-                        <tr role="button" tabIndex="0" data-index={index} key={"classroom-"+index} onClick={() => {this.props.showClassroom(classroom.code)}}>
-                            <td>{classroom.classroom_name}</td>
-                            <td>{classroom.code}</td>
-                            <td>{classroom.num_students}</td>
-                        </tr>
-                    );
-                });
+            return (
+                <div id="classrooms-table-container">
+                    <h1>Your Classrooms</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Code</th>
+                                <th>Number of Students</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderRows()}
+                        </tbody>
+                    </table>
+                </div>
+            )
         } else {
             return null;
         }
@@ -51,23 +81,7 @@ class ClassOverview extends React.Component {
                     <button className="btn btn-lg readingaid-btn" onClick={this.props.logout}>Logout</button>
                 </div>
                 <div className="clear-fix"></div>
-                { this.state.classrooms.length > 0 &&
-                    <div id="classrooms-table-container">
-                        <h1>Your Classrooms</h1>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Code</th>
-                                    <th>Number of Students</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.renderRows()}
-                            </tbody>
-                        </table>
-                    </div>
-                }
+                {this.state.retrievedClassrooms && this.renderTable()}
             </div>
         );
     }
