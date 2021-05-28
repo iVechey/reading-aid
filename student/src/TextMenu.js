cc7986e6eda879c71a77e69fcc54324b72464cc3
Your Name <you@example.com>
2021-05-28 10:04:11 -0700

    debugged componentDidMount

--- student/src/TextMenu.js
+++ student/src/TextMenu.js
@@ -8,61 +8,40 @@ import { Button } from "react-bootstrap"
 class TextMenu extends React.Component {
 
     constructor(props) {
-        super(props)
-        this.getAssignedTextIds = this.getTexts.bind(this);
+        super(props);
         this.displayTexts = this.displayTexts.bind(this);
-        this.getTexts = this.getTexts.bind(this);
         this.state = {
             user: firebase.auth().currentUser,
-            assignedTextIds: [],
             texts: [],
         }
     }
 
-    componentDidMount() {
-        this.getAssignedTextIds();
-        this.getTexts();
-    }
-
-    getAssignedTextsIds() {
-        firebase.database().ref('students').child(this.state.user.uid).child('texts').on('value', (snapshot) => {
-            if(snapshot.exists()) {
-                snapshot.forEach((child => {
-                    this.state.assignedTextIds.push(child)
-                }))
+    async componentDidMount() {
+        const snapshot = await firebase.database().ref('students').child(this.state.user.uid).child('texts').get();
+        if(snapshot.exists()) {
+            this.setState({ texts: snapshot.val() });
+            if(this.state.texts) {
+                for (const [id, text] of Object.entries(this.state.texts)) {
+                    const child = await firebase.database().ref('texts').child(id).child("title").get();
+                    text.title = child.val();
+                }
             }
-        })
+            this.setState({ loading: false });
+        }
     }
-
-    getTexts() {
-        firebase.database().ref('texts').on('value', (snapshot) => {
-            if(snapshot.exists()) {
-                snapshot.forEach((child => {
-                    if (this.state.assignedTextIds.includes(child.textId)) {
-                        this.state.texts.push(child)
-                    }
-                }))
-            }
-        })
-    }
-
-    // TODO: display logo for each text assigned - if timesRead < 3 clickable
-    //       (on click redirect to specific text page)
-    //       and if timesRead >= 3 grayed out and not clickable
 
     displayTexts() {
 
-            const texts = Object.values(this.state.texts);
+        const texts = this.state.texts;
 
-            return texts.texts ? (
-                this.state.texts.forEach((text => {
-                    return (
-                        <Button value={text.title} size="large"/>
-                    )
-                }))
-            ) : (
-                <h2>no texts have been assigned</h2>
-            )
+        if (texts) {
+            return Object.values(texts).map((text) => {
+                return <h2>"{text.title}"</h2>
+            });
+        } else {
+            <h2>no texts have been assigned</h2>
+        }
+
     }
 
     render() {
