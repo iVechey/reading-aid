a3b2ebb201e94db3343b20be78965c4918e73b35
iVechey <62487905+iVechey@users.noreply.github.com>
2021-06-04 20:51:53 -0700

    Update Recorder.js

--- Pointer/src/Recorder.js
+++ Pointer/src/Recorder.js
@@ -1,58 +1,61 @@
 //https://www.npmjs.com/package/audio-react-recorder
 //this is where the code came form, it's not super complex
 
-import React, { Component } from 'react'
-import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+import React, { Component } from "react";
+import AudioReactRecorder, { RecordState } from "audio-react-recorder";
 
 class Recorder extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      recordState: null
-    }
+      recordState: null,
+      audioData: null
+    };
   }
-
-  start = () => {
+  componentDidMount() {
     this.setState({
       recordState: RecordState.START
     })
   }
- 
+
   stop = () => {
     this.setState({
       recordState: RecordState.STOP
-    })
-  }
- 
+    });
+  };
   //audioData contains blob and blobUrl
-  onStop = (audioData) => {
+  onStop = (data) => {
+    //this.setState({recordState: RecordState.STOP})
+    this.setState({
+      audioData: data
+    })
     //just here to show you everything
-    console.log('audioData', audioData)
     //i think this is what we will need
-    console.log(audioData.url)
-  }
- 
+    //console.log(audioData.url);
+    this.props.audio(data)
+  
+
+  };
+
   render() {
-    const { recordState } = this.state
-    
-    //we may have to have the students 
+    const { recordState } = this.state;
+
+    //we may have to have the students
     //click a button to start the recording
     //you make canvas Width/Height both 0
     return (
       <div>
-        Recorder: Check console for the audio file
-        <AudioReactRecorder 
-        canvasWidth = {20}
-        canvasHeight = {20}
-        backgroundColor = "red"
-        foreGroundColor = "red"
-        state={recordState} onStop={this.onStop} />
-        
-        <button onClick={this.start}>Start</button>
-        <button onClick={this.stop}>Stop</button>
+        <AudioReactRecorder
+          canvasWidth={20}
+          canvasHeight={20}
+          backgroundColor="red"
+          foreGroundColor="red"
+          state={recordState}
+          onStop={this.onStop}
+        />
       </div>
-    )
+    );
   }
 }
 export default Recorder;
