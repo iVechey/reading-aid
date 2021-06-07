cb7c88d04c15f199c5e034a085f14af352495009
iVechey <62487905+iVechey@users.noreply.github.com>
2021-06-07 09:43:27 -0700

    Update Recognition.js

--- student/src/Recognition.js
+++ student/src/Recognition.js
@@ -1,12 +1,20 @@
 import SpeechRecognition, {
   useSpeechRecognition
 } from "react-speech-recognition";
+/*this will recognize the voice and convert that to text, similar how siri works. We will have a list of commands of the text
+which will call a function checking if they got the word correctly
 
+Page:
+https://www.npmjs.com/package/react-speech-recognition
+*/
+
+//Ideally this would be in a component. However, there is some strange thing about hooks which is beyond my expertise. You'll see an error
+//Feel free to check out the error.
 function Recognition(props) {
   const wordList = props.wordList;
   const commands = [{command: "2", callback: () => props.callBack({command: "to"})}];
-  //need to get from firebase, no clue how to access that yet
-
+  //map each word into a specific command command: word said callback: function call... we want the same function which checks the word said
+  //with the taget word
   wordList.map((word) => {
     commands.push({
       command: word.replace(/[.,/#!$%^&*;:'{}=\-_`~()]/g, ""),
@@ -14,8 +22,9 @@ function Recognition(props) {
     });
     return null;
   });
-
+  //this will start the speech recognition with the given commands
   useSpeechRecognition({ commands });
+  //drawback of the package: if user stops talking the recognition will stop listening, not ideal for students. 
   SpeechRecognition.startListening({ continuous: true });
 
   return null;
